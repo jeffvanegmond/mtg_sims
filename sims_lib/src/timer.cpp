4cf051b2d2c890b83d8c965e1c77562b6a3c4a92
J.A. van Egmond <j.a.vanegmond@tudelft.nl>
2016-07-29 12:25:02 +0200

    Add timer class

---
+++ sims_lib/src/timer.cpp
@@ -0,0 +1,15 @@
+#include "timer.h"
+
+namespace MTGSims {
+
+Timer::Timer() : beg_(clock_::now()) {}
+
+void Timer::reset() {
+	beg_ = clock_::now();
+}
+
+double Timer::elapsed() const {
+	return std::chrono::duration_cast<second_>(clock_::now() - beg_).count();
+}
+
+}
